fe5a3685133eedbfb2081d37f0c4e133623c84aa
heshanu <heshanu97@gmail.com>
2022-07-22 01:06:38 +0530

    changed to postgress

--- src/main/java/com/company/SpringbootrestapiApplication.java
+++ src/main/java/com/company/SpringbootrestapiApplication.java
@@ -2,8 +2,6 @@ package com.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 
 @SpringBootApplication
 public class SpringbootrestapiApplication {
