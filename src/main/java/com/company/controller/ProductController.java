8033439a15a1ae1116fa5e0fe663711fb59237f2
heshanu <heshanu97@gmail.com>
2025-10-01 14:03:51 +0530

    update controller with deleteProductThree

--- src/main/java/com/company/controller/ProductController.java
+++ src/main/java/com/company/controller/ProductController.java
@@ -106,6 +106,14 @@ public class ProductController {
 	}
 
 	@DeleteMapping("/{id}")
+	public ResponseEntity<Map<String, Object>> deleteProductSix(@PathVariable("id") Long id) {
+		productService.deleteProduct(id);
+		Map<String, Object> map = new HashMap<>();
+		map.put("status", "Product deleted!");
+		return new ResponseEntity<>(map, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Map<String, Object>> deleteProductThree(@PathVariable("id") Long id) {
 		productService.deleteProduct(id);
 		Map<String, Object> map = new HashMap<>();
@@ -128,5 +136,9 @@ public class ProductController {
 		map.put("status", "Product deleted!");
 		return new ResponseEntity<>(map, HttpStatus.OK);
 	}
+
+
+
+
 
 }
